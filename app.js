79b85aff3b6ef1d071c3c5a93416d6e599218bab
Hollis Colburn <hollismcolburn@gmail.com>
2022-05-18 19:24:06 -0700

    Simplified forms and food info pages

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Pet = require('./models/pet');
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://hollis:XyMZL4ZtAkSlBrbp@pet-food-calculator.qwhze.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// Function to serve all static files
-// inside public directory.
+// middleware & static files
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
+app.use(express.urlencoded({ extended: true }));
 
-// listen for requests
-app.listen(3000);
-
+// routes
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/petinfo', (req, res) => {
-    res.render('index');
+    res.render('petinfo');
 });
 
-app.get('/dryfood', (req, res) => {
-    res.render('dryfood');
-});
+// app.post('/petinfo', (req, res) => {
+//     const pet = new Pet(req.body);
 
-app.get('/wetfood', (req, res) => {
-    res.render('wetfood');
-});
+//     pet.save()
+//         .then((result) => {
+//             res.redirect('/dryfood')
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         });
+// })
 
 app.get('/feedingplan', (req, res) => {
     res.render('feedingplan');
