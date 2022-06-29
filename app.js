caee980d6639c8cffb46d20cc8b97025eec0c2b2
Hollis Colburn <hollismcolburn@gmail.com>
2022-06-29 15:53:56 -0700

    Start ejs; create working buttons

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const Pet = require('./models/pet');
 // express app
 const app = express();
 
+
+
 // connect to mongodb
 const dbURI = 'mongodb+srv://' + user + ':' + password + '@pet-food-calculator.qwhze.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
+
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -40,8 +43,24 @@ app.get('/petinfo', (req, res) => {
 //         });
 // })
 
-app.get('/feedingplan', (req, res) => {
-    res.render('feedingplan');
+// app.get('/feedingplan', (req, res) => {
+//     res.render('feedingplan');
+// });
+
+app.get('/feedingplan', function(req, res) {
+    var exampleInfo = {
+        petName: 'Hoagie',
+        petSpecies: 'Cat',
+        calorieGoal: 225,
+        mealsPerDay: 2,
+        dryFoodName: "Hill's Perfect Weight",
+        caloriesPerCup: 300,
+        wetFoodName: "Hill's Perfect Weight",
+        caloriesPerCan: 64,
+        cansPerDay: 1
+    }
+
+    res.render('feedingplan', { exampleInfo });
 });
 
 // 404 page
---
+++ private/mongodb.js
@@ -0,0 +1,3 @@
+const dbURI = 'mongodb+srv://hollis:XyMZL4ZtAkSlBrbp@pet-food-calculator.qwhze.mongodb.net/?retryWrites=true&w=majority';
+const user = 'hollis';
+const password = 'XyMZL4ZtAkSlBrbp'
